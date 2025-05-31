5b0520e40fbdb8a0b83902f8d7b99c65687e232f
jinqianxiaogege <qianjin2023@shanghaitech.edu.cn>
2025-05-31 22:04:52 +0800

    first try

---
+++ src/assembly/example.h
@@ -0,0 +1,9 @@
+#ifndef LAB12_STARTER_EXAMPLE_H
+#define LAB12_STARTER_EXAMPLE_H
+
+extern int selected_scenario;  // Global variable for assembly access
+
+void draw();
+int scenario_select(int initial_choice);
+
+#endif //LAB12_STARTER_EXAMPLE_H
